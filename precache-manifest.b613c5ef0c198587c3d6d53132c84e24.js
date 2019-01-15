f9b1daca4cc52c1903fba19e74b0102ce6333efa
kingrayhan <rayhan095@gmail.com>
2019-01-15 12:19:10 +0600

    Updates

---
+++ precache-manifest.b613c5ef0c198587c3d6d53132c84e24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f7bdb7235d0ad9912781",
+    "url": "/static/js/main.f7bdb723.chunk.js"
+  },
+  {
+    "revision": "d094bdb788d8899c1bcf",
+    "url": "/static/js/1.d094bdb7.chunk.js"
+  },
+  {
+    "revision": "d094bdb788d8899c1bcf",
+    "url": "/static/css/1.160e04f1.chunk.css"
+  },
+  {
+    "revision": "29d7f1aa6ed534e6c3bfcf92d274a00b",
+    "url": "/index.html"
+  }
+];
